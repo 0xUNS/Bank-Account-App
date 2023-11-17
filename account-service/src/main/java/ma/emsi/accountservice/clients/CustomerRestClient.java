59f47b1b182f154c9fd815a6589101c02772a0bc
0xuns <0xuns@github.com>
2023-11-17 21:30:12 +0100

    Add OpenFeign feature

--- account-service/src/main/java/ma/emsi/accountservice/clients/CustomerRestClient.java
+++ account-service/src/main/java/ma/emsi/accountservice/clients/CustomerRestClient.java
@@ -1,4 +1,18 @@
 package ma.emsi.accountservice.clients;
 
+import ma.emsi.accountservice.model.Customer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.cloud.openfeign.FeignClient;
+
+import java.util.List;
+
+@FeignClient(name = "CUSTOMER-SERVICE")
 public interface CustomerRestClient {
+
+    @GetMapping("/customers/{id}")
+    Customer findCustomerById(@PathVariable Long id);
+
+    @GetMapping("/customers")
+    List<Customer> allCustomers();
 }
--- account-service/src/main/java/ma/emsi/accountservice/web/AccountRestController.java
+++ account-service/src/main/java/ma/emsi/accountservice/web/AccountRestController.java
@@ -1,20 +1,25 @@
 package ma.emsi.accountservice.web;
 
 import lombok.AllArgsConstructor;
+import ma.emsi.accountservice.clients.CustomerRestClient;
 import ma.emsi.accountservice.entities.BankAccount;
+import ma.emsi.accountservice.model.Customer;
 import ma.emsi.accountservice.repositories.BankAccountRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/accounts")
 @AllArgsConstructor
+@EnableFeignClients
 public class AccountRestController {
     private BankAccountRepository bankAccountRepository;
+    private CustomerRestClient customerRestClient;
     @GetMapping
     public List<BankAccount> bankAccounts() {
         return bankAccountRepository.findAll();
@@ -22,7 +27,10 @@ public class AccountRestController {
 
     @GetMapping("/{id}")
     public BankAccount bankAccount(@PathVariable String id) {
-        return bankAccountRepository.findById(id).orElse(null);
+        BankAccount bankAccount = bankAccountRepository.findById(id).orElse(null);
+        Customer customer = customerRestClient.findCustomerById(bankAccount.getCustomerId());
+        bankAccount.setCustomer(customer);
+        return bankAccount;
     }
 
 }

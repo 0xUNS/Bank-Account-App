5f5b36a0bd232c8a5a4b5eff57996f80a02e4a50
0xuns <0xuns@github.com>
2023-12-11 13:08:04 +0100

    add config service

--- account-service/src/main/java/ma/emsi/accountservice/AccountServiceApplication.java
+++ account-service/src/main/java/ma/emsi/accountservice/AccountServiceApplication.java
@@ -1,17 +1,20 @@
 package ma.emsi.accountservice;
 
+import ma.emsi.accountservice.clients.CustomerRestClient;
 import ma.emsi.accountservice.entities.BankAccount;
 import ma.emsi.accountservice.enums.AccountType;
 import ma.emsi.accountservice.repositories.BankAccountRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootApplication
+@EnableFeignClients
 public class AccountServiceApplication {
 
     public static void main(String[] args) {
@@ -19,13 +22,13 @@ public class AccountServiceApplication {
     }
 
     @Bean
-    CommandLineRunner commandLineRunner(BankAccountRepository bankAccountRepository) {
-        return args -> bankAccountRepository.saveAll(List.of(
-                BankAccount.builder().currency("MAD").balance(12000).createAt(LocalDate.now()).type(AccountType.SAVING_ACCOUNT).customerId(1L).build(),
-                BankAccount.builder().currency("MAD").balance(7000).createAt(LocalDate.now()).type(AccountType.CURRENT_ACCOUNT).customerId(3L).build(),
-                BankAccount.builder().currency("MAD").balance(8500).createAt(LocalDate.now()).type(AccountType.SAVING_ACCOUNT).customerId(3L).build(),
-                BankAccount.builder().currency("MAD").balance(16000).createAt(LocalDate.now()).type(AccountType.SAVING_ACCOUNT).customerId(2L).build(),
-                BankAccount.builder().currency("MAD").balance(5000).createAt(LocalDate.now()).type(AccountType.CURRENT_ACCOUNT).customerId(1L).build()
-        ));
+    CommandLineRunner commandLineRunner(BankAccountRepository bankAccountRepository, CustomerRestClient customerRestClient) {
+        return args -> customerRestClient.allCustomers().forEach(customer ->
+                        bankAccountRepository.saveAll(List.of(
+                                BankAccount.builder().currency("MAD").balance(Math.random()*10000).createAt(LocalDate.now()).type(AccountType.SAVING_ACCOUNT).customerId(customer.getId()).build(),
+                                BankAccount.builder().currency("MAD").balance(Math.random()*10000).createAt(LocalDate.now()).type(AccountType.CURRENT_ACCOUNT).customerId(customer.getId()).build(),
+                                BankAccount.builder().currency("MAD").balance(Math.random()*10000).createAt(LocalDate.now()).type(AccountType.SAVING_ACCOUNT).customerId(customer.getId()).build()
+                        ))
+                );
     }
 }
--- account-service/src/main/java/ma/emsi/accountservice/clients/CustomerRestClient.java
+++ account-service/src/main/java/ma/emsi/accountservice/clients/CustomerRestClient.java
@@ -1,5 +1,6 @@
 package ma.emsi.accountservice.clients;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import ma.emsi.accountservice.model.Customer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +12,22 @@ import java.util.List;
 public interface CustomerRestClient {
 
     @GetMapping("/customers/{id}")
+    @CircuitBreaker(name = "customerService", fallbackMethod = "getDefaultCustomer")
     Customer findCustomerById(@PathVariable Long id);
 
     @GetMapping("/customers")
     List<Customer> allCustomers();
+
+    default Customer getDefaultCustomer(Long id, Exception exception) {
+        return Customer.builder()
+                .id(id)
+                .firstName("Not available")
+                .lastName("Not available")
+                .email("Not available")
+                .build();
+    }
+
+    default List<Customer> getAllCustomers(Exception exception) {
+        return List.of();
+    }
 }
